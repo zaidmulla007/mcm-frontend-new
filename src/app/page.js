243ee16dc8099c02aafeddc065e440f7ba5810c9
zaidmulla007 <zaidmulla1122@gmail.com>
2025-09-10 17:56:19 +0530

    updated codes

--- src/app/page.js
+++ src/app/page.js
@@ -945,7 +945,7 @@ export default function Home() {
         >
           <h2 className="text-4xl md:text-5xl font-bold mb-4">
             <span className="bg-gradient-to-r from-purple-400 via-pink-400 to-blue-400 bg-clip-text text-transparent">
-              What's Trending
+              What&apos;s Trending
             </span>
           </h2>
           <p className="text-gray-300 text-xl mb-8">Top 5 Mentioned Coins in 24H</p>
